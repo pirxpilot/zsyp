8af6bb0f69acbe3965862500dd6644419007f047
Damian Krzeminski <pirxpilot@furkot.com>
2019-07-13 22:33:25 -0600

    add basic implementation
    listen on port, parse, and log

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-module.exports = require('./lib/zsyp');
+require('dotenv').config({ path: '/etc/default/zsyp' });
+
+const connect = require('@pirxpilot/connect');
+const router = require('./lib/csp');
+
+const PORT = process.env.ZSYP_SERVER_PORT || 3090;
+
+const app = connect();
+
+app.use(router());
+
+
+module.exports = app;
+
+if (!module.parent) {
+  app.listen(PORT);
+  console.log('Listening on port', PORT);
+}
