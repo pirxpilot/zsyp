713cfaed02e74af4876f04cf3f02939ddff7d1aa
Damian Krzeminski <pirxpilot@furkot.com>
2019-07-14 07:23:00 -0600

    extract logger

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ require('dotenv').config({ path: '/etc/default/zsyp' });
 
 const connect = require('@pirxpilot/connect');
 const router = require('./lib/csp');
+const makeLogger = require('./lib/logger');
 
 const PORT = process.env.ZSYP_SERVER_PORT || 3090;
 
 const app = connect();
+const log = makeLogger();
 
+app.use(function (req, res, next) {
+  req.log = log;
+  next();
+});
 app.use(router());
 
 
