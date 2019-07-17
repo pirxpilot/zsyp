3710e8d42d5f2664922f7b9fcfdcb7e892308746
Damian Krzeminski <pirxpilot@furkot.com>
2019-07-17 12:20:58 -0600

    fix reports domain filtering

---
+++ test/filter.js
@@ -0,0 +1,49 @@
+const test = require('tape');
+const makeFilter = require('../lib/filter');
+
+test('filter with no params', function (t) {
+  const req = {
+    body: {
+      'csp-report': {
+        'document-uri': 'https://example.com/login'
+      },
+    }
+  };
+
+  const filter = makeFilter({});
+
+  t.plan(1);
+
+  filter(req, null,  () => t.pass());
+});
+
+test('filter match', function (t) {
+  const req = {
+    body: {
+      'csp-report': {
+        'document-uri': 'https://example.com/login'
+      },
+    }
+  };
+
+  const filter = makeFilter({ domains: '^example' });
+
+  t.plan(1);
+
+  filter(req, null, () => t.pass());
+});
+
+test('filter no match', function (t) {
+  const req = {
+    body: {
+      'csp-report': {
+        'document-uri': 'https://example.com/login'
+      },
+    }
+  };
+
+  const filter = makeFilter({ domains: 'google.com' });
+
+  filter(req, null, () => t.fail());
+  t.end();
+});
