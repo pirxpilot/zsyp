d3e923b7bc2ad486ea6ef65194966fd5cd183083
Damian Krzeminski <pirxpilot@furkot.com>
2022-04-06 22:01:29 +0200

    add support for `/event` path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: '/etc/default/zsyp' });
 
 const connect = require('@pirxpilot/connect');
 const mniam = require('mniam');
+const { json } = require('body-parser');
 const router = require('./lib/router');
 
 const {
@@ -13,7 +14,9 @@ const {
 const app = connect();
 const db = mniam.db(database);
 
-app.use(router({ domains, db }));
+app.use(json({ limit: 5000, type: [ '*/json', 'application/csp-report' ] }));
+app.use('/csp', router({ domains, db, name: 'csp' }));
+app.use('/event', router({ domains, db, name: 'event' }));
 
 module.exports = app;
 
