e9fea5fdf801b9163700d5cb4335974d279335c4
Damian Krzeminski <pirxpilot@furkot.com>
2022-04-06 22:23:12 +0200

    refactor app to allow for testing

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 require('dotenv').config({ path: '/etc/default/zsyp' });
 
-const connect = require('@pirxpilot/connect');
-const mniam = require('mniam');
-const { json } = require('body-parser');
-const router = require('./lib/router');
+const logger = require('./lib/logger');
+const makeApp = require('./lib/app');
 
 const {
   ZSYP_PORT: PORT = 3090,
-  ZSYP_DOMAINS: domains,
-  ZSYP_DB: database = 'mongodb://localhost/zsyp'
 } = process.env;
 
-const app = connect();
-const db = mniam.db(database);
-
-app.use(json({ limit: 5000, type: [ '*/json', 'application/csp-report' ] }));
-app.use('/csp', router({ domains, db, name: 'csp' }));
-app.use('/event', router({ domains, db, name: 'event' }));
+const app = makeApp({
+  logger
+});
 
 module.exports = app;
 
