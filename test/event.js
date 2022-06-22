42dcbc76f5d42fb618ba69bb04d819bee056d74b
Damian Krzeminski <pirxpilot@furkot.com>
2022-06-22 13:03:37 +0200

    add `_hash` id to errors
    it is computed as a short hash of the stack content and kept in DB as
    `ObjectID`
    
    also store the original stack as `orig_stack` for debugging

--- test/event.js
+++ test/event.js
@@ -1,6 +1,7 @@
 const test = require('tape');
 require('./env');
 
+const { ObjectId } = require('mongodb');
 const { converter } = require('../lib/event');
 
 test('converter for event', async function (t) {
@@ -10,16 +11,16 @@ test('converter for event', async function (t) {
   t.deepEqual(converted.meta, { name: 'event' });
 });
 
-
 test('converter for error', async function (t) {
+  const stack = `TypeError: app.forceError is not a function
+    at makeApp (http://example.com/app.js:1:305)
+    at app.use (http://example.com/app.js:1:600)`;
   const error = {
     type: 'error',
     an: 'zapp',
     av: '1.0.0',
     message: "Uncaught TypeError: app.forceError is not a function",
-    stack: `TypeError: app.forceError is not a function
-      at makeApp (http://example.com/app.js:1:305)
-      at app.use (http://example.com/app.js:1:600)`
+    stack
   };
   const converted = await converter(error);
   t.deepEqual(converted.item, {
@@ -29,7 +30,9 @@ test('converter for error', async function (t) {
     stack: [
       ['lib/app.js', 17, 2, 'makeApp'],
       ['lib/app.js', 25, 0, 'app.use']
-    ]
+    ],
+    org_stack: stack,
+    _hash: new ObjectId('67e90d7a6cac7dd838a76a68')
   });
   t.deepEqual(converted.meta, { name: 'error' });
 });
